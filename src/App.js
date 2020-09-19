bca78e601c55f2a36823914e3769f01471b09d63
Sojung Oh <sojung3514@gmail.com>
2020-09-19 17:05:52 +0900

    Node.js - state

--- src/App.js
+++ src/App.js
@@ -1,37 +1,23 @@
-import React from 'react';
+import React from "react";
 
-function Food({name, picture}){
-  return <div>
-    <h2>I like {name}</h2>
-    <img src = {picture} alt = {name} />
-  </div>
-}
-
-const foodILike = [
-  {
-    id: 1,
-    name: "pattbingsu",
-    img: "https://www.koreanbapsang.com/wp-content/uploads/2020/07/DSC2381-500x500.jpg"
-  },
-  {
-    id: 2,
-    name: "kimchijeon",
-    img: "https://www.koreanbapsang.com/wp-content/uploads/2010/02/DSC0780-2-500x500.jpg"
-  },
-  {
-    id: 3,
-    name: "spagetti",
-    img: "https://www.koreanbapsang.com/wp-content/uploads/2020/04/DSC0488-1-500x500.jpg"
+class App extends React.Component{
+  state = {
+    isLoading: true,
+    movies: []
   }
-];
 
-function App() {
-  return (
-  <div>
-    {foodILike.map(dish=>(
-    <Food key = {dish.id} name = {dish.name} picture = {dish.img} />))};
-  </div>
-  );
-    }
+  componentDidMount(){
+    setTimeout(()=>{
+      this.setState({isLoading : false});
+    }, 6000)
+  }
+
+  render(){
+    const {isLoading} = this.state;
+    return <div>
+      {isLoading ? "Loading..." : "We are ready!"}
+    </div>
+  }
+}
 
 export default App;
