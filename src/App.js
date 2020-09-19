16cdffcd2c919678cc70784e93bda23fcf040255
Sojung Oh <sojung3514@gmail.com>
2020-09-19 14:03:06 +0900

    JSX&props

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+function Food({name, picture}){
+  return <div>
+    <h2>I like {name}</h2>
+    <img src = {picture} alt = {name} />
+  </div>
+}
+
+const foodILike = [
+  {
+    id: 1,
+    name: "pattbingsu",
+    img: "https://www.koreanbapsang.com/wp-content/uploads/2020/07/DSC2381-500x500.jpg"
+  },
+  {
+    id: 2,
+    name: "kimchijeon",
+    img: "https://www.koreanbapsang.com/wp-content/uploads/2010/02/DSC0780-2-500x500.jpg"
+  },
+  {
+    id: 3,
+    name: "spagetti",
+    img: "https://www.koreanbapsang.com/wp-content/uploads/2020/04/DSC0488-1-500x500.jpg"
+  }
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+    {foodILike.map(dish=>(
+    <Food key = {dish.id} name = {dish.name} picture = {dish.img} />))};
+  </div>
   );
-}
+    }
 
 export default App;
