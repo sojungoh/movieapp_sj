0dfdf37834a03383c34e8b559293322c82764e16
Sojung Oh <sojung3514@gmail.com>
2020-09-28 00:08:44 +0900

    Updates

---
+++ precache-manifest.c02e000e03ac986a10616c0a49b68d2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43b7e707cc35e61723f2a321c098eef8",
+    "url": "/movieapp_sj/index.html"
+  },
+  {
+    "revision": "4daf4dcc6a71d44f965a",
+    "url": "/movieapp_sj/static/css/main.c8dc0729.chunk.css"
+  },
+  {
+    "revision": "079afa20fcdebbda9e08",
+    "url": "/movieapp_sj/static/js/2.68703e8b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movieapp_sj/static/js/2.68703e8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4daf4dcc6a71d44f965a",
+    "url": "/movieapp_sj/static/js/main.54a8bafd.chunk.js"
+  },
+  {
+    "revision": "7d6711d24767bb0875c2",
+    "url": "/movieapp_sj/static/js/runtime-main.0fabad25.js"
+  }
+]);
